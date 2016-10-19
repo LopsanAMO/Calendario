290ed485f5d10f1295bee809880538c9d2b2a4c7
LopsanAMO <makiki789@gmail.com>
2016-10-19 04:04:38 -0500

    index

--- usuario/views.py
+++ usuario/views.py
@@ -11,7 +11,7 @@ class Registar(View):
         context = {
             'form': form
         }
-        return render
+        return render(request, template_name, context)
 
     def post(self, request):
         form = RegistroForm(request.POST)
@@ -37,5 +37,58 @@ class Login(View):
         context = {
             'form': form
         }
+        return render(request, template_name, context)
 
     def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+            if user is not None:
+                login(request, user)
+                return redirect('usuario:calendario')
+            else:
+                return HttpResponseRedirect('.')
+        else:
+            return HttpResponseRedirect('.')
+
+class Index(View):
+    def get(self, request):
+        template_name = 'index.html'
+        registro = RegistroForm()
+        login = LoginForm()
+            'r_form': registro,
+            'l_form': login,
+        }
+        return render(request, template_name, context)
+
+    def post(self, request):
+        action = request.POST.get('action')
+        if action == 'registro':
+            form = RegistroForm(request.POST)
+            usu = Usuario()
+            if form.is_valid():
+                form.save()
+                try:
+                    usu.user = User.object.get(request.POST.get('username'))
+                    usu.tipo = 'usuario'
+                    usu.save()
+                    user = authenticate(username=request.POST.get('username'),password=request.POST.get('password'))
+                    login(request, user)
+                    return redirect('usuario:index')
+                except:
+                    return HttpResponseRedirect('.')
+            else:
+                return HttpResponseRedirect('.')
+        elif action == 'login':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+                if user is not None:
+                    login(request, user)
+                    return redirect('usuario:calendario')
+                else:
+                    return HttpResponseRedirect('.')
+            else:
+                return HttpResponseRedirect('.')
+        else:
+            return HttpResponseRedirect('.')
