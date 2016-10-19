f3e504c0960f4af868b5abcc5070bf1581d1f14c
LopsanAMO <makiki789@gmail.com>
2016-10-18 23:25:56 -0500

    Creamos los modelos para el usuario

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(models.Model):
+    user = models.ForeignKey(User, null=False, blank=False)
+    boleta = models.CharField(null=False ,blank=False, max_length=10)
+
+class qr(models.Model):
+    user models.ForeignKey(User, null=False, blank=False)
+    codigo = models.ImageField(null=True, blak=True)
