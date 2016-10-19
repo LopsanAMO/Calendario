7b6f907289d03f6977a436cd57acfd956b2a5f33
LopsanAMO <makiki789@gmail.com>
2016-10-19 01:31:54 -0500

    creando mas modelos

--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from materias.models import Materia
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tarea(models.Model):
+    nombre = models.CharField(blank=False, null=False, max_length=100)
+    materia = models.ForeignKey(Materia, blank=False, null=False)
+    fecha_entrega = models.DateField(blank=False, null=False)
+    prioridad = models.CharField(max_length=50, blank=False, null=False)
+
+class Alarma(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+    tarea = models.ForeignKey(Tarea, blank=False, null=False)
+
+class hora_alarma(models.Model):
+    hora = models.DateField(blank=False, null=False)
+    fecha = models.DateField(blank=False, null=False)
+    alarma = models.ForeignKey(Alarma, blank=False, null=False,)
+
+
+class Compañero(models.Model):
+    nombre = models.ForeignKey(User, blank=True, null=True)
+    tarea = models.ForeignKey(Tarea, blank=True, null=True)
